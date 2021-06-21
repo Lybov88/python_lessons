0137836d81ed0181bd56f9535b3f024cfde214b9
Lybov88 <eL_Pi@mail.ru>
2021-06-21 23:25:33 +0900

    created 7.functions

--- 7.functions.py
+++ 7.functions.py
@@ -33,4 +33,50 @@ def func_3(arg_1, arg_2=100):
     print(arg_1 * arg_2)
 
 
-func_3(20, 10)
+# func_3(20, 10)
+# func_3(100)
+# func_3(3.14)
+
+# 2 вариант. Функция, возвращающая данные
+def func_4(arg_1, arg_2):
+    res = arg_1 + arg_2
+    return res
+
+
+# d = func_4(10, 10)
+
+def func_5(x, y):
+    res_1 = x * y
+    res_2 = x / y
+    return res_1, res_2, x
+
+
+#  первый способ приема данных
+d_1 = func_5(10, 2)
+
+#  второй способ приема данных
+a, b, c = func_5(10, 2)
+
+# print(d_1)
+# print(d_1[0])
+
+# print(a, b, c)
+# print(b)
+
+
+#  *** Безымянные функции (лямбда-выражения, лямбда-функции)
+
+# Особенности:
+#  - всегда обладают аргументами
+#  - всегда возвращает результат
+
+#  Пример 1. Лямбда внутри генератора списка
+my_list = [(lambda arg_1: arg_1.upper())(i) for i in "hello"]
+
+# Пример 2. Словарь лямбда-выражений
+my_lambdas = {
+    "*": lambda arg_1, arg_2: arg_1 * arg_2,
+    "+": lambda w, z: w + z
+}
+print(my_lambdas["+"](5, 2))
+print(my_lambdas["*"](5, 2))
